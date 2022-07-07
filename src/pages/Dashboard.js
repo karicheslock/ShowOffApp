de27060223802c0d0f7f96449242c76565d88fb2
Kari Cheslock <karisd5274@gmail.com>
2022-07-07 13:28:19 -0700

    Additional dashboard styling

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -30,19 +30,19 @@ function Dashboard() {
   return (
     <div className="container">
       <Navbar />
-      <div className="bg-amber-50 h-full">
+      <div className="bg-amber-50 h-full flex flex-col items-center w-screen">
           {userCollectionsArray.map((userCollection) => {
             return (
-              <div key={userCollection.id}>
-                <div>             
-                  <p>{userCollection.title}</p>
-                  <p>{userCollection.description}</p>
+              <div key={userCollection.id} className="flex flex-col border-2 border-amber-900 rounded w-2/5 m-4 py-1 items-center">
+                <div className="border-4 border-double border-amber-900 w-3/4 text-center">             
+                  <p className="text-3xl text-amber-900"><span className="font-blaka-hollow text-amber-400"><span className="font-blaka-hollow text-amber-400 text-5xl">T</span>ITLE: </span> <span className="font-bold">{userCollection.title}</span></p>
+                  <p className="text-xl text-amber-900"><span className="font-blaka-hollow text-amber-400"><span className="font-blaka-hollow text-amber-400 text-3xl">D</span>ESCRIPTION: </span> {userCollection.description}</p>
                 </div>
-                <div className="flex flex-wrap">
+                <div className="flex flex-wrap items-center justify-center">
                   {userCollection.imageArray.slice(0,3).map((image) => (
-                      <img src={image} alt="User Collection Item" className="w-1/12 h-max" />
+                      <img src={image} alt="User Collection Item" className="w-1/5 h-max rounded mx-2 border-2 border-amber-900 my-4 hover:w-2/5" />
                   ))}
-                  {(userCollection.imageArray.length > 3) && <Link to='/profile/:username'>Click here to see the rest of this collection</Link>}
+                  {(userCollection.imageArray.length > 3) && <Link to='/profile/:username' className="text-amber-900 underline decoration-solid decoration-amber-900 hover:text-amber-400 hover:decoration-amber-400">Click here to see the rest of this collection</Link>}
                 </div>
               </div>
             )
